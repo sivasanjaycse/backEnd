87dd681e13543cee3a34364db52911c43f6b01d6
sivasanjaycse <sivasanjaidisco@gamil.com>
2025-09-24 19:16:10 +0530

    dlkfn

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL }));
 // Increase payload size limit for Base64 images
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: false, limit: '10mb' }));
